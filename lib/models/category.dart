a05ecbc775faa824c954ed60bad8b817c4618fd4
Diógenes Silva <diosaves@gmail.com>
2022-07-21 21:57:06 -0300

    Category Item Component

---
+++ lib/models/category.dart
@@ -0,0 +1,51 @@
+import 'dart:convert';
+
+class Category {
+  final String? name;
+  final String? picture;
+  Category({
+    required this.name,
+    required this.picture,
+  });
+
+  Category copyWith({
+    required String name,
+    required String picture,
+  }) {
+    return Category(
+      name: name ?? this.name,
+      picture: picture ?? this.picture,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'picture': picture,
+    };
+  }
+
+  factory Category.fromMap(Map<String, dynamic> map) {
+    return Category(
+      name: map['name'],
+      picture: map['picture'],
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Category.fromJson(String source) => Category.fromMap(json.decode(source));
+
+  @override
+  String toString() => 'Category(name: $name, picture: $picture)';
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is Category && other.name == name && other.picture == picture;
+  }
+
+  @override
+  int get hashCode => name.hashCode ^ picture.hashCode;
+}
